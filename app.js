7902af393b9dd962e69cd385b6e4987428914f3e
ellsbee <katebroniahurley@gmail.com>
2024-05-28 16:07:35 +0100

    completes second task 02-get-topics

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const {getTopics} = require("./controllers/controller")
+const app = express()
+
+app.get('/api/topics', getTopics);
+
+app.use((req, res) => {
+    res.status(404).send({msg: 'Not Found'})
+})
+
+module.exports = app;
